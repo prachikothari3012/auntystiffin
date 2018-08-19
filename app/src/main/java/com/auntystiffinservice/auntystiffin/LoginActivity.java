7544b1743386ba1ca7f02a7c31a2df7bbef835a2
Prachi Kothari <prachikothari3012@gmail.com>
2018-08-19 13:01:54 +0530

    test commit

--- app/src/main/java/com/auntystiffinservice/auntystiffin/LoginActivity.java
+++ app/src/main/java/com/auntystiffinservice/auntystiffin/LoginActivity.java
@@ -54,6 +54,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
+
+
+
+
     private UserLoginTask mAuthTask = null;
 
     // UI references.
